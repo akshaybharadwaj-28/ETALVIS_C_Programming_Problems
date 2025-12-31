ddf4a1633ec9e4d5dda79ddfcf2736f16ce8f6e2
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2025-12-31 22:42:30 +0530

    Create problem6.c
    Wrote solution code for problem 6.

---
+++ c_level_3/problem6.c
@@ -0,0 +1,18 @@
+//Question:- Get a number from the user and check if the number is greater than or equal to 50. If yes, then print "Success", otherwise print "Failure".
+//Example:- Input:45 Output:Failure    Input:56789 Output:Success
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num>=50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
