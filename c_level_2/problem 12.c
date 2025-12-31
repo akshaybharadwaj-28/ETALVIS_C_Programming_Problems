2a322c5568cc2e4f1d60f77f41c5d7006d1b41eb
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2025-12-31 18:35:50 +0530

    Create problem 12.c
    Wrote solution code for problem 12.

---
+++ c_level_2/problem 12.c
@@ -0,0 +1,19 @@
+//Question:- Get a two digit number from the user and check if the digit 1 is greater than or equal to digit 0. If yes, then print 1; otherwise print 0.
+//Example:- Input:66 Output:1    Input:67 Output:1    Input:98 Output:0
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int num,temp;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    temp=num%10;
+    num=num/10;
+    switch (num>=temp){
+        case 1 : printf("Result is 1");
+                  break;
+        default : printf("Result is 0");
+    }
+}
