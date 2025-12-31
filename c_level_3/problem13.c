93ab2cc0f54abb413fb24e49871557703b74d02b
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2025-12-31 23:02:15 +0530

    Create problem13.c
    Wrote a solution code for problem 13.

---
+++ c_level_3/problem13.c
@@ -0,0 +1,21 @@
+//Question:- Get a four digit number from the user and check if the digit 1 and digit 0 are the same and if digit 1 and digit 0 are different. If yes, then print "Success"; otherwise print "Failure".
+//Example:- Input:988 Output:1    Input:567 Output:0
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int num,temp;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    temp=num%10;
+    num=num/10;
+    num=num%10;
+    if(num==temp){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
