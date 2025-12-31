0c1e97eaceabd342e5b7889156a7f22a5dc76cd0
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2025-12-31 22:46:28 +0530

    Create problem7.c
    Wrote a solution code for problem 7.

---
+++ c_level_3/problem7.c
@@ -0,0 +1,20 @@
+//Question:- Get a two digit number from the user and check if the digit 0 and digit 1 are identical. If yes, then print "Success", otherwise print "Failure".
+//Example:- Input:55 Output:Success    Input:56 Output:Failure
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int num,temp;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    temp=num%10;
+    num=num/10;
+    if(num==temp){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
