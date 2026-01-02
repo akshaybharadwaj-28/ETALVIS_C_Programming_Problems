6c986c9207c08e0408148a2735e0f5be7b7dd8dc
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-02 17:15:12 +0530

    Create problem8.c
    Wrote a solution code for problem 8.

---
+++ c_level_5/problem8.c
@@ -0,0 +1,20 @@
+//Question: Write a loop program to print two digit even numbers, whose sum of digits are 6.
+/* Answer:  24
+            42
+            60
+*/
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int i,d0,d1;
+    for(i=10;i<100;i++){
+        d0=i%10;
+        d1=i/10;
+        if(i%2==0 && d0+d1==6){
+            printf("%d\n", i);
+        }
+    }
+}
