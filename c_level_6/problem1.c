3e88805b7f34a5207ad841894364464afc08a88c
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-05 23:11:34 +0530

    Create problem1.c
    Wrote a solution code for problem 1.

---
+++ c_level_6/problem1.c
@@ -0,0 +1,18 @@
+//Question: Write a program to print 1 to 5 one by one.
+/* Answer:  1
+            2
+            3
+            4
+            5
+*/
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int i=1;
+    while(i<6){
+        printf("%d\n", i);
+        i++;
+  
