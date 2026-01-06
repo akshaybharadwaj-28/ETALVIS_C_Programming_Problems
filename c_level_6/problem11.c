1a66d3c2be70596f2db4214c4043c17e0727d989
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-06 23:32:55 +0530

    Create problem11.c
    Wrote a solution code for problem 11.

---
+++ c_level_6/problem11.c
@@ -0,0 +1,18 @@
+//Question: Write a program to get a number from user print the total number of digits in that number.
+//Input: 123456 Output:6    Input:76895439 Output:8    Input:675 Output:3
+
+//Code
+
+#include <stdio.h>
+
+int main() {
+    int i,count=0,digit;
+    printf("Enter a number: ");
+    scanf("%d", &i);
+    while(i!=0){
+        digit=i%10;
+        count++;
+        i=i/10;
+    }
+    printf("%d", count);
+}
