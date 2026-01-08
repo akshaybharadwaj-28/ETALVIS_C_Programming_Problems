ae23c33b32e31c80da3e3b521b7213b9c9674f19
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-08 21:24:40 +0530

    Create problem24.c
    Wrote a solution code for problem 24.

---
+++ c_level_7/problem24.c
@@ -0,0 +1,26 @@
+//Question: Write a program to get a number from the user, print the total number of two digit perfect square numbers in the number.
+//Input:163496481 Output:4    Input:364925  Output:4
+
+//Function name : disp_two_digit_square
+
+//Code
+
+#include <stdio.h>
+int disp_two_digit_square(int num);
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    disp_two_digit_square(num);
+}
+int disp_two_digit_square(int num){
+    int i,digit,count=0;
+    while(num>10){
+        digit=num%100;
+        for(i=0;i<digit;i++){
+            if(i*i==digit){
+                count++;
+            }
+        }
+        num=num/10;
+    }
