7e13df17e805976ddc6c39a8caf9757bcce5b68f
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-11 00:10:33 +0530

    Create problem19.c
    Wrote a solution code for problem 19.

---
+++ c_level_8/problem19.c
@@ -0,0 +1,27 @@
+//Question: Write a program to get three numbers from user print the LCM of those numbers.
+//Input: 8 12 18 Output: 72 
+
+//Function name : disp_LCM3
+
+//Code
+
+#include <stdio.h>
+int disp_LCM3(int, int, int);
+int main(){
+    int num1,num2,num3,y;
+    printf("Enter 3 nos: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    y=disp_LCM3(num1, num2, num3);
+    printf("%d",y);
+}
+
+int disp_LCM3(int num1, int num2, int num3){
+    int lcm,i;
+    for(i=num1;i>=num1;i++){
+        if(i%num1==0 && i%num2==0 && i%num3==0){
+            lcm=i;
+            break;
+        }
+    }
+    return lcm;
+}
