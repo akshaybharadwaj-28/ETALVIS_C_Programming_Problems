7d91e7ff44efb8b1e035f5ad639ebcce5c9b8b5c
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-13 12:09:14 +0530

    Create problem13.c
    Wrote a solution code for problem 13.

---
+++ c_level_9/problem13.c
@@ -0,0 +1,28 @@
+//Question: Get multiple numbers from the user and store them in an array. Stop getting numbers when the number is 0. If 4 numbers are entered and first and last numbers are equal, print "Success" else "Failure".
+
+//Code
+
+#include <stdio.h>
+int main(){
+    int i,j,num,a[20],count=0;
+    printf("Enter multiple numbers: ");
+    for(i=0;i<20;i++){
+        scanf("%d",&a[i]);
+        if(a[i]==0){
+            break;
+        }
+    }
+    i=0;
+    while(a[i]!=0){
+        if(a[i]){
+            count++;
+        }
+        i++;
+    }
+    if(count==4 && a[0]==a[3]){
+        printf("Success\n");
+    }
+    else{
+        printf("Failure");
+    }
+}
