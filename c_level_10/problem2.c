309f9b0e4a43133cdfaae2c548203387162d5c38
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-14 15:02:59 +0530

    Create problem2.c
    Wrote a solution code for problem 2.

---
+++ c_level_10/problem2.c
@@ -0,0 +1,11 @@
+//Question: Get a number and print its ASCII value
+
+//Code
+
+#include <stdio.h>
+int main(){
+    char a;
+    printf("Enter a number: ");
+    scanf("%c",&a);
+    printf("%d",a);
+}
