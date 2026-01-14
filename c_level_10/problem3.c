3c0b4d8b717f4101a7d4775a52a73cb47424fdb6
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-14 15:06:22 +0530

    Create problem3.c
    Wrote a solution code for problem 3.

---
+++ c_level_10/problem3.c
@@ -0,0 +1,11 @@
+//Question: Get a string and print the same
+
+//Code
+
+#include <stdio.h>
+int main(){
+    char a[10];
+    printf("Enter a string: ");
+    scanf("%s",a);
+    printf("%s",a);
+}
