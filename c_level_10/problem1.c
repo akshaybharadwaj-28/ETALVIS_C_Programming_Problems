60f951f8739a4665f8adadbefc25a5cbbdfffb5b
Akshay S <114243255+akshaybharadwaj-28@users.noreply.github.com>
2026-01-14 14:56:00 +0530

    Create problem1.c
    Wrote a solution code for problem 1.

---
+++ c_level_10/problem1.c
@@ -0,0 +1,11 @@
+//Question: Get an alphabet and print its ASCII value
+
+//Code
+
+#include <stdio.h>
+int main(){
+    char a;
+    printf("Enter a character: ");
+    scanf("%c",&a);
+    printf("%d",a);
+}
